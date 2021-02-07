60a597c513b3637d91095b330210fdd6208b9c74
UBC Student <sgu14@annacis.students.cs.ubc.ca>
2021-02-06 18:59:04 -0800

    swap method giving seg fault

--- pa1/main.cpp
+++ pa1/main.cpp
@@ -30,13 +30,13 @@ int main() {
   im.readFromFile("images/kh.png");
   //im.readFromFile("images/terrain.png");
   //im.readFromFile("images/ronMueck.png");
-  //im.readFromFile("images/sun.png");
+  // im.readFromFile("images/sun.png");
 
   Chain c(im, 5);
   // randomly scramble the blocks.
   // c.scramble();
   // or test swapping a few blocks
-  //  c.testSwap(3,4); // swap the 3rd and 4th blocks.
+  c.testSwap(3,4); // swap the 3rd and 4th blocks.
   //  c.testSwap(0,1);
   //  c.testSwap(1,0);
   //  c.testSwap(0,4);
